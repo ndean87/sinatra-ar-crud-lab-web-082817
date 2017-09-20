61da327cea433637235af3fdeabae50f8358aafb
github username <github email address>
2017-09-19 20:22:41 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,57 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do 
-    
+  get '/' do
+
   end
+
+  get '/posts/new' do
+    erb :new
+  end
+
+  post '/posts' do
+    @name = params[:name]
+    @content = params[:content]
+    @post = Post.create(name: @name, content: @content)
+    @posts = Post.all
+    erb :index
+    # redirect to '/posts'
+  end
+
+    get '/posts' do
+      @posts = Post.all
+      erb :index
+    end
+
+  get '/posts/:id' do
+    @post = Post.find(params[:id])
+    erb :show
+  end
+
+  patch '/posts/:id' do
+    @name = params[:name]
+    @content = params[:content]
+    @post = Post.find(params[:id])
+    @post.update(name: @name, content: @content)
+    erb :show
+  end
+
+  get '/posts/:id/edit' do
+    @post = Post.find(params[:id])
+    erb :edit
+  end
+
+  delete '/posts/:id/delete' do
+    @post = Post.all.find(params[:id])
+    @post.delete
+    # Post.destroy(@post)
+    erb :delete
+  end
+
+  # add delete button to show page
+  # add content to delete controller so it takes button and acts on it
+  # sends you to a new page delete.erb that says page has been deleted
+
+
+
 end
